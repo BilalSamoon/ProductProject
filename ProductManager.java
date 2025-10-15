c8e1f6d91f375474004e3d026aca3a14322dab58
BilalSamoon <gilljohnson213@gmail.com>
2025-10-14 20:22:36 -0400

    Added discount feature by Bilal

--- ProductManager.java
+++ ProductManager.java
@@ -1,5 +1,11 @@
 public class ProductManager {
+
     public static void main(String[] args) {
         System.out.println("Product Manager initialized.");
+    }
+
+    
+    public double calculateDiscount(double price, double percent) {
+        return price - (price * percent / 100);
     }
 }
